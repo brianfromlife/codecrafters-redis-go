f163eab8124d5913910d68dcf164f0e5d7e0ca86
brianfromlife <brianfromlife@gmail.com>
2024-03-29 21:23:03 -0400

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -18,7 +18,8 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	c, err := l.Accept()
+	c.Write([]byte("+PONG\r\n"))
 
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
